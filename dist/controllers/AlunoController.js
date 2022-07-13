2920f4a558826ce5deb186005a3075b4f16c80ca
Phelipe <phelipedsantos@hotmail.com>
2022-07-13 16:44:01 -0300

    build

---
+++ dist/controllers/AlunoController.js
@@ -0,0 +1,78 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _Aluno = require('../models/Aluno'); var _Aluno2 = _interopRequireDefault(_Aluno);
+var _Photo = require('../models/Photo'); var _Photo2 = _interopRequireDefault(_Photo);
+
+class AlunoController {
+  async index(req, res) {
+    const alunos = await _Aluno2.default.findAll({
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [_Photo2.default, 'id', 'DESC']],
+      include: {
+        model: _Photo2.default,
+        attributes: ['url', 'filename', 'originalname'],
+      },
+    });
+    res.json(alunos);
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await _Aluno2.default.create(req.body);
+      return res.json(aluno);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) return res.status(400).json({ errors: ['Faltando ID'] });
+
+      const aluno = await _Aluno2.default.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+        order: [['id', 'DESC'], [_Photo2.default, 'id', 'DESC']],
+        include: {
+          model: _Photo2.default,
+          attributes: ['url', 'filename', 'originalname'],
+        },
+      });
+      if (!aluno) return res.status(400).json({ errors: ['Aluno não existe'] });
+
+      return res.json(aluno);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) return res.status(400).json({ errors: ['Faltando ID'] });
+
+      const aluno = await _Aluno2.default.findByPk(id);
+      if (!aluno) return res.status(400).json({ errors: ['Aluno não existe'] });
+
+      const alunoAtualizado = await aluno.update(req.body);
+      return res.json(alunoAtualizado);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) return res.status(400).json({ errors: ['Faltando ID'] });
+
+      const aluno = await _Aluno2.default.findByPk(id);
+      if (!aluno) return res.status(400).json({ errors: ['Aluno não existe'] });
+
+      await aluno.destroy();
+      return res.json(`Aluno: ${aluno.nome} apagado com sucesso.`);
+    } catch (error) {
+      return res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+}
+
+exports. default = new AlunoController();
--- server.js
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-const port = 3001;
-app.listen(port, () => {
-  console.log(`\nEscutando na porta ${port}`);
-  console.log(`CTRL + Clique em http://localhost:${port}`);
-});
